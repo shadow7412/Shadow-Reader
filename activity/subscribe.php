80f7a60d025257c894f0df4f28a3d3449d4911d8
Steven Huf <steven@thinkbrick.(none)>
2013-03-14 15:51:15 +1100

    More psuedo train-of-thought code.

---
+++ activity/subscribe.php
@@ -0,0 +1,19 @@
+<?php
+//For when a user wishes to subscribe to a new feed
+
+//Check to make sure it isn't already in here.
+
+//Grab site.
+
+//Check to see if site is RSS.
+
+//Check to see if site has link to RSS feed
+
+//Check to make sure it isn't already in here
+
+//if not, Add new feed to database
+
+//Add user to that feed
+
+//Report to user whether it worked or not
+?>
---
+++ exe/updateThreads.php
@@ -0,0 +1,14 @@
+<?php
+//this will probably take a while to execute.
+
+//log in to mysql
+//Get list of threads from mysql
+//for each thread,
+//if thread does not update often, and it has been checked recently, skip
+//download rss
+//for each rss:
+//if the current one exists, break here (move to next feed).
+//add current one to db
+
+//possibly do some metrics here for interests sake
+?>
