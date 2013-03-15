9f81f2f5cdebcd9bb49eb23ad04ecaf02579e9e8
Steven Huf <steven@thinkbrick.(none)>
2013-03-15 13:28:18 +1100

    Added a title. Oops.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 ﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xml:lang="en-US">
 <head>
+  <title>Shadow Reader</title>
   <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
   <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/jquery-ui.min.js"></script>
   <script type="text/javascript" src="include/smoothscroll.js"></script>
