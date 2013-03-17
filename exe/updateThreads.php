a2b11369db3b93ce1639dcc27b72484be1d8dc16
Shadow <huffy7412@gmail.com>
2013-03-18 09:57:03 +1100

    Metrics now taken for scraping
    Moved feed.class.php to includes.

--- exe/updateThreads.php
+++ exe/updateThreads.php
@@ -1,14 +1,21 @@
 #!/usr/bin/env php
 <?php
-if(php_sapi_name() != 'cli') echo "<pre>";
+$buffer = "";
+function output(){
+	global $buffer;
+	$d = implode(func_get_args());echo "> ",$d;$buffer.=$d;
+}
+if(php_sapi_name() != 'cli') echo "<pre>"; //We don't need to keep the pretags... That's why we echo instead of output.
 //this will probably take a while to execute.
 //log in to mysql
+$feedcount = $errors = $newitems = 0;
 require "../include/db.php";
-require "feed.class.php";
+require "../include/feed.class.php";
 //Get list of threads from mysql
+if($db->connect_error) output($db->connect_error);
 $feeds = $db->query("SELECT * FROM `rssfeeds`");
 
-if(!$feeds) echo $db->error;
+if(!$feeds) output($db->error);
 
 //set up prepared query for checking to see if we already have an entry (by using the date and title of the item) as well as one for adding items
 $additem = $db->prepare("INSERT INTO `rssitems` (`feed`,`link`,`subject`,`date`,`content`) VALUES (?, ?, ?, ?, ?)");
@@ -18,10 +25,15 @@ $checkitem = $db->prepare("SELECT `id` FROM `rssitems` WHERE date=? AND subject=
 $checkitem->bind_param("ss",$date,$subject);
 
 while($row = $feeds->fetch_assoc()){
+  $feedcount++;
 //if thread does not update often, and it has been checked recently, Perhaps look for patterns in history?
   //download rss
-  echo "Scraping ",$row['title'],"...\n";
+  output("Scraping ",$row['title'],"...\n");
   $rss = Feed::loadRss($row['url']);
+  if(!$rss){
+	$errors++;
+	output("Failed to scrape :(\n");
+  }
   //for each rss:
   foreach($rss->item as $item){
   //if the current one exists, break here (move to next feed).
@@ -35,15 +47,18 @@ while($row = $feeds->fetch_assoc()){
 	$checkitem->store_result();
 
 	if($checkitem->num_rows==0){
-	      echo "    Adding item: ",$row['title']," - ",$subject,"... ",$additem->execute()?"Success":"Failed",$db->error,"\n";
+	      output("\tAdding item",++$newitems,": ",$row['title']," - ",$subject,"... ",$additem->execute()?"Success":"Failed",$db->error,"\n");
 	} else {
-	      echo "    Found entry we already have - skipping rest of thread.\n";
+	      output("\tFound entry we already have - skipping rest of thread.\n");
 	      break;
 	}
   }
 }
-echo "Scrape Complete.";
+output("Scrape Complete - ",$time=time()-$_SERVER['REQUEST_TIME']," seconds.");
 $additem->close();
 $checkitem->close();
+$db->query("INSERT INTO `scrapemetrics` (`feeds`,`time`,`items`,`errors`,`details`) VALUES ($feedcount,$time,$newitems,$errors,'"
+	.$db->real_escape_string($buffer)
+	."')");
 if(php_sapi_name() != 'cli') echo "</pre>";
 ?>
